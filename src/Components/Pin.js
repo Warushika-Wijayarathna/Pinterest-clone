d10dfe770ab650ac31096d20b47a3279f451aebe
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2024-08-19 08:02:49 +0530

    Adding sample image and share icon

--- src/Components/Pin.js
+++ src/Components/Pin.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Pin({pinSize}) {
-    return <div className={`pin ${pinSize}`}></div>
+    return <div className={`pin ${pinSize}`}>
+        <img className="mainPic" src="https://cdn.pixabay.com/photo/2024/08/05/21/19/lion-8947711_1280.jpg" 
+        alt=""
+        />
+
+        <div className="content">
+            <h3>Sample Name</h3>
+            
+            <div className="search">
+            <img src= "https://firebasestorage.googleapis.com/v0/b/printerest-clone-773aa.appspot.com/o/right-arrow-symbol%20(1).png?alt=media&token=b2c60ed8-2462-48df-b5d7-610cde874f91"
+            alt="search" />
+          </div>
+        </div>
+    </div>
 }
 
 export default Pin;
