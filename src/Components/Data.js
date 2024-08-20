2f0a00dd2027a309ead82ec36aed67da0ec6150d
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2024-08-20 06:50:41 +0530

    Updating Images

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -2,57 +2,57 @@ const Data = [
     {
       id: 1,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/12/01/17/28/branch-6838719_960_720.jpg",
-      name: "Vladvictoria",
-      link: "https://pixabay.com/photos/branch-ornaments-lights-tree-6838719/",
+        "https://cdn.pixabay.com/photo/2023/08/07/19/47/water-lily-8175845_1280.jpg",
+      name: "Water Lily",
+      link: "https://pixabay.com/photos/water-lily-flower-blossom-bloom-8175845/",
       size: "small",
     },
     {
       id: 2,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/12/05/21/39/christmas-balls-6848782_960_720.jpg",
-      name: "Mammiya",
-      link: "https://pixabay.com/photos/christmas-balls-christmas-advent-6848782/",
+        "https://cdn.pixabay.com/photo/2024/07/02/06/19/rain-8866774_1280.png",
+      name: "Rain",
+      link: "https://pixabay.com/illustrations/rain-raining-weather-umbrella-draw-8866774/",
       size: "medium",
     },
     {
       id: 3,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/07/29/03/58/child-5446839_960_720.jpg",
-      name: "Zhangliams",
-      link: "https://pixabay.com/photos/child-boy-run-alley-street-plants-5446839/",
+        "https://cdn.pixabay.com/photo/2024/07/01/14/31/pasta-8865344_1280.jpg",
+      name: "Pasta",
+      link: "https://pixabay.com/photos/pasta-food-spaghetti-noodles-8865344/",
       size: "large",
     },
     {
       id: 4,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/14/09/14/cat-1822979_960_720.jpg",
-      name: "Mshyun",
-      link: "https://pixabay.com/photos/cat-christmas-tree-ornaments-1822979/",
+        "https://cdn.pixabay.com/photo/2023/11/26/23/43/horse-8414411_1280.jpg",
+      name: "Horse",
+      link: "https://pixabay.com/photos/horse-pony-horses-muzzle-8414411/",
       size: "medium",
     },
     {
       id: 5,
       imgSrc:
         "https://cdn.pixabay.com/photo/2021/11/02/10/46/lemur-6762935_960_720.jpg",
-      name: "Jansheldan",
+      name: "Lemur",
       link: "https://pixabay.com/photos/lemur-animal-wilderness-nature-6762935/",
       size: "large",
     },
     {
       id: 6,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/10/13/07/43/couple-6705694_960_720.jpg",
-      name: "Jupilu",
-      link: "https://pixabay.com/photos/couple-sundown-field-sunset-6705694/",
+        "https://cdn.pixabay.com/photo/2018/03/04/17/23/tiger-3198598_1280.jpg",
+      name: "Tiger",
+      link: "https://pixabay.com/photos/tiger-cat-mammal-carnivore-3198598/",
       size: "small",
     },
     {
       id: 7,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_960_720.jpg",
-      name: "Stergo",
-      link: "https://pixabay.com/photos/fantasy-butterflies-mushrooms-2049567/",
+        "https://cdn.pixabay.com/photo/2023/12/15/03/11/basket-to-the-sea-8449952_1280.jpg",
+      name: "Basket To The Sea",
+      link: "https://pixabay.com/photos/basket-to-the-sea-go-basket-8449952/",
       size: "medium",
     },
     {
@@ -66,121 +66,121 @@ const Data = [
     {
       id: 9,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/11/26/20/44/lantern-6826687_960_720.jpg",
-      name: "Lasrs Nissen",
-      link: "https://pixabay.com/photos/lantern-sea-sky-beach-water-6826687/",
+        "https://cdn.pixabay.com/photo/2024/02/24/00/05/concert-hall-8593024_1280.jpg",
+      name: "Concert Hall",
+      link: "https://pixabay.com/photos/concert-hall-building-landmark-8593024/",
       size: "medium",
     },
     {
       id: 10,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2021/11/30/17/06/tree-6835828_960_720.jpg",
-      name: "Jell Wllington",
-      link: "https://pixabay.com/photos/tree-snow-winter-christmas-branch-6835828/",
+        "https://cdn.pixabay.com/photo/2023/12/12/15/47/owl-8445460_960_720.jpg",
+      name: "Owl",
+      link: "https://pixabay.com/photos/owl-bird-northern-long-eared-owl-8445460/",
       size: "medium",
     },
     {
       id: 11,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg",
-      name: "Chraecker",
-      link: "https://pixabay.com/photos/owl-bird-animal-bird-of-prey-50267/",
+        "https://cdn.pixabay.com/photo/2024/07/29/02/28/snake-8928741_1280.jpg",
+      name: "Snake",
+      link: "https://pixabay.com/photos/snake-green-tree-python-python-8928741/",
       size: "small",
     },
     {
       id: 12,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/06/07/10/47/elephant-2380009_960_720.jpg",
-      name: "Comfreak",
-      link: "https://pixabay.com/photos/elephant-watering-hole-baby-elephant-2380009/",
+        "https://cdn.pixabay.com/photo/2024/07/18/21/29/darts-8905121_1280.jpg",
+      name: "Darts",
+      link: "https://pixabay.com/photos/darts-sport-dart-board-arrow-8905121/",
       size: "medium",
     },
     {
       id: 13,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
-      name: "Sasinet",
-      link: "https://pixabay.com/photos/woman-kid-rain-leaf-umbrella-1807533/",
+        "https://cdn.pixabay.com/photo/2024/08/05/21/19/lion-8947711_1280.jpg",
+      name: "Lion",
+      link: "https://pixabay.com/photos/lion-big-cat-animal-feline-8947711/",
       size: "large",
     },
     {
       id: 14,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2019/08/19/07/45/dog-4415649_960_720.jpg",
-      name: "Hauad",
-      link: "https://pixabay.com/photos/dog-corgi-pets-cute-rain-4415649/",
+        "https://cdn.pixabay.com/photo/2022/04/30/08/27/baltic-sea-7165113_1280.jpg",
+      name: "Baltic Sea",
+      link: "https://pixabay.com/photos/baltic-sea-sunset-taking-photos-7165113/",
       size: "medium",
     },
     {
       id: 15,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014619_960_720.jpg",
-      name: "Chau min",
-      link: "https://pixabay.com/photos/japan-osaka-night-asia-landmark-2014619/",
+        "https://cdn.pixabay.com/photo/2023/07/23/14/13/frog-8145165_960_720.jpg",
+      name: "Frog",
+      link: "https://pixabay.com/photos/frog-amphibians-fauna-species-8145165/",
       size: "large",
     },
     {
       id: 16,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2015/10/22/17/45/leaf-1001679_960_720.jpg",
-      name: "Steven",
-      link: "https://pixabay.com/photos/leaf-green-foliage-green-leaves-1001679/",
+        "https://cdn.pixabay.com/photo/2024/02/15/14/31/donkey-8575524_1280.jpg",
+      name: "Donkey",
+      link: "https://pixabay.com/photos/donkey-desert-siwa-egypt-oasis-8575524/",
       size: "medium",
     },
     {
       id: 17,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2018/05/16/22/27/rose-3407234_960_720.jpg",
-      name: "Rosee",
-      link: "https://pixabay.com/photos/rose-red-drops-rain-macro-nature-3407234/",
+        "https://cdn.pixabay.com/photo/2024/02/11/13/33/kumquats-8566508_1280.jpg",
+      name: "Kumquats",
+      link: "https://pixabay.com/photos/kumquats-citrus-fruits-8566508/",
       size: "medium",
     },
     {
       id: 18,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2018/01/29/19/00/park-3116883_960_720.jpg",
-      name: "Arcoin",
-      link: "https://pixabay.com/photos/park-bench-night-evening-fog-mist-3116883/",
+        "https://cdn.pixabay.com/photo/2024/08/17/14/14/flowers-8975928_960_720.jpg",
+      name: "Flowers",
+      link: "https://pixabay.com/photos/flowers-spring-nature-flora-8975928/",
       size: "large",
     },
     {
       id: 19,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/22/23/44/porsche-1851246_960_720.jpg",
-      name: "Pexel",
-      link: "https://pixabay.com/photos/porsche-car-brake-lights-1851246/",
+        "https://cdn.pixabay.com/photo/2024/08/17/13/18/ai-generated-8975841_960_720.jpg",
+      name: "AI Generated Jesus Christ",
+      link: "https://pixabay.com/photos/ai-generated-jesus-christ-8975841/",
       size: "small",
     },
     {
       id: 20,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/06/05/20/41/girl-1438138_960_720.jpg",
-      name: "Xusenru",
-      link: "https://pixabay.com/photos/girl-umbrella-rain-park-autumn-1438138/",
+        "https://cdn.pixabay.com/photo/2024/08/17/16/44/purple-8976249_1280.jpg",
+      name: "Purple Flowers",
+      link: "https://pixabay.com/photos/purple-flowers-violet-botanical-8976249/",
       size: "large",
     },
     {
       id: 21,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/03/16/20/20/man-2150164_960_720.jpg",
-      name: "Fifalinana",
-      link: "https://pixabay.com/photos/man-waterfalls-bathe-bathing-falls-2150164/",
+        "https://cdn.pixabay.com/photo/2024/08/17/09/41/road-8975491_1280.jpg",
+      name: "Road",
+      link: "https://pixabay.com/photos/road-girl-dog-walk-beach-seaside-8975491/",
       size: "large",
     },
     {
       id: 22,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/29/11/12/rain-1869119_960_720.jpg",
-      name: "Pexel",
-      link: "https://pixabay.com/photos/rain-woman-brunette-portrait-1869119/",
+        "https://cdn.pixabay.com/photo/2024/08/17/16/44/squirrel-8976246_1280.jpg",
+      name: "Squirrel",
+      link: "https://pixabay.com/photos/squirrel-rodent-animal-mammal-8976246/",
       size: "medium",
     },
     {
       id: 23,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2016/11/19/11/11/hands-1838659_960_720.jpg",
-      name: "Pexel",
-      link: "https://pixabay.com/photos/hands-plant-soil-grow-growing-1838659/",
+        "https://cdn.pixabay.com/photo/2024/08/17/09/32/beetle-8975462_1280.jpg",
+      name: "Beetle",
+      link: "https://pixabay.com/photos/beetle-cicada-insect-dipteran-8975462/",
       size: "medium",
     },
     {
@@ -194,49 +194,49 @@ const Data = [
     {
       id: 25,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2013/02/20/10/32/snail-83672_960_720.jpg",
-      name: "Larisa",
-      link: "https://pixabay.com/illustrations/snail-drops-rain-raindrops-mollusk-83672/",
+        "https://cdn.pixabay.com/photo/2024/08/17/13/15/woman-8975830_1280.jpg",
+      name: "Business Women",
+      link: "https://pixabay.com/photos/woman-model-business-woman-8975830/",
       size: "samll",
     },
     {
       id: 26,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/08/30/09/28/buildings-5528981_960_720.jpg",
-      name: "China Town",
-      link: "https://pixabay.com/photos/buildings-houses-street-city-rain-5528981/",
+        "https://cdn.pixabay.com/photo/2024/08/16/10/18/plane-8973307_1280.jpg",
+      name: "Plane",
+      link: "https://pixabay.com/photos/plane-toy-modelling-miniature-8973307/",
       size: "small",
     },
     {
       id: 27,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2017/03/12/02/20/triangle-2136288_960_720.jpg",
-      name: "Pyramid",
-      link: "https://pixabay.com/illustrations/triangle-sky-abstract-wallpaper-2136288/",
+        "https://cdn.pixabay.com/photo/2024/08/16/02/43/woman-8972558_1280.jpg",
+      name: "Women In Silhouette",
+      link: "https://pixabay.com/photos/woman-dress-beach-silhouette-8972558/",
       size: "large",
     },
     {
       id: 28,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/04/02/23/01/rain-4996916_960_720.jpg",
-      name: "Pyou",
-      link: "https://pixabay.com/photos/rain-street-tables-cafe-flowers-4996916/",
+        "https://cdn.pixabay.com/photo/2024/08/15/00/04/woman-8969908_1280.jpg",
+      name: "Women With Sunflowers",
+      link: "https://pixabay.com/photos/woman-sunflower-beautiful-flowers-8969908/",
       size: "small",
     },
     {
       id: 29,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/05/19/13/05/heart-5190672_960_720.jpg",
-      name: "Heart",
-      link: "https://pixabay.com/photos/heart-window-rain-drops-love-5190672/",
+        "https://cdn.pixabay.com/photo/2024/08/15/03/52/monkeys-8970148_1280.jpg",
+      name: "Monkeys",
+      link: "https://pixabay.com/photos/monkeys-primate-animal-8970148/",
       size: "small",
     },
     {
       id: 30,
       imgSrc:
-        "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg",
-      name: "cat",
-      link: "https://pixabay.com/photos/cat-kitten-pet-kitty-young-cat-551554/",
+        "https://cdn.pixabay.com/photo/2024/08/15/03/27/penguin-8970117_1280.jpg",
+      name: "Penquin",
+      link: "https://pixabay.com/photos/penguin-mammal-swimming-water-zoo-8970117/",
       size: "large",
     },
   ];
